770c556710bf1a27f23e322d113ac7210fe55e76
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-08-02 12:40:39 +0200

    [Fix] Javadoc in Method of OperationDAO

--- src/main/java/Diginamic/TP5JPA/DAO/Specific/Comptes/Operations/OperationDAO.java
+++ src/main/java/Diginamic/TP5JPA/DAO/Specific/Comptes/Operations/OperationDAO.java
@@ -12,8 +12,18 @@ public class OperationDAO extends GenericDAO<Operation, Long> {
     public OperationDAO(){
         super(Operation.class);
     }
+    /**
+      * Retrieves a list of {@link Operation} entities associated with the specified account ID.
+      * @param accountId the ID of the account for which to retrieve operations.
+      * @return a {@link List} of {@link Operation} entities that are associated with the given account ID.
+      * If no operations are found, an empty list is returned.
+      * @throws IllegalArgumentException if the provided {@code accountId} is {@code null}.
+     */
 
-        public List<Operation> findByAccountId(Long accountId) {
+    public List<Operation> findByAccountId(Long accountId) {
+        if (accountId == null) {
+            throw new IllegalArgumentException("Account ID cannot be null");
+        }
         EntityManager em = DatabaseUtil.getEntityManager();
         String jpql = "SELECT o FROM Operation o WHERE o.compte.id = :accountId";
         TypedQuery<Operation> query = em.createQuery(jpql, Operation.class);
